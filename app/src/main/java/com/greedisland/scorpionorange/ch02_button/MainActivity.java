313701217c0f8409b8ee49e5963adf29ecfc8d9b
scorpionorange <scorpionorange@126.com>
2019-01-01 20:29:33 +0800

    Initial commit

---
+++ app/src/main/java/com/greedisland/scorpionorange/ch02_button/MainActivity.java
@@ -0,0 +1,30 @@
+package com.greedisland.scorpionorange.ch02_button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int size = 30;
+
+    public void bigger(View v){
+        TextView txv = (TextView) findViewById(R.id.txv);
+        txv.setTextSize(++size);
+    }
+
+    public void smaller(View v){
+        if(size > 10){
+            TextView txv = (TextView) findViewById(R.id.txv);
+            txv.setTextSize(--size);
+        }
+    }
+
+}
